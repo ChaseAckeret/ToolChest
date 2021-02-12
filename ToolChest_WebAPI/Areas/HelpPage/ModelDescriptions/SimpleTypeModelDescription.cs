de4fb0c96083818f252466fd46ea8e6308d0ad0c
Chase Ackeret <chaseackeret@gmail.com>
2021-02-11 19:24:38 -0500

    Add project files.

---
+++ ToolChest_WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ToolChest_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ToolChest_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToolChest_WebAPI.WebApiApplication" Language="C#" %>
