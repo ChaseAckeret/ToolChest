4562a81453071b89c1411b9236560eba2826d468
Chase Ackeret <chaseackeret@gmail.com>
2021-02-16 21:23:28 -0500

    Added properties for owner class

--- ToolChest_Data/Owner.cs
+++ ToolChest_Data/Owner.cs
@@ -9,5 +9,14 @@ namespace ToolChest_Data
 {
     public class Owner
     {
+        [Key]
+        public int Id { get; set; }
+
+        public virtual List<OwnerRating> Rating { get; set; }
+
+        public virtual List<Tool> Tools { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
     }
 }
