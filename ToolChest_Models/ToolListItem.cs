c86b0d88b358312f03bc8fd1b23d3137fb4ef498
JForestal1 <jforestal1@gmail.com>
2021-02-23 19:07:41 -0500

    tool creates working

--- ToolChest_Models/ToolListItem.cs
+++ ToolChest_Models/ToolListItem.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ToolChest_Data.Tool;
 
 namespace ToolChest_Models
 {
     public class ToolListItem
     {
+        public int ToolID { get; set; }
+        public decimal HourlyRate { get; set; }
+
+        public decimal DailyRate { get; set; }
+
+        public ConditionType ToolCondition { get; set; }
+
+        public int ToolCatalogItem;
     }
 }
